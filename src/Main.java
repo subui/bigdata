b6f60be1f8a49b9c35a1bdf2d048a4ad785409b2
subui <subui.bk@gmail.com>
2018-01-04 13:19:16 +0700

    src\Main.java

--- src/Main.java
+++ src/Main.java
@@ -27,16 +27,16 @@ public class Main {
 		data.add(6, 4, 5);
 		CF cf = new CF(data, 2, (vector1, vector2) -> {
 			double dotProduct = 0.0;
-		    double norm1 = 0.0;
-		    double norm2 = 0.0;
-		    for (int i = 0; i < vector1.length; i++) {
-		        dotProduct += vector1[i] * vector2[i];
-		        norm1 += Math.pow(vector1[i], 2);
-		        norm2 += Math.pow(vector2[i], 2);
-		    }   
-		    return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
+			double norm1 = 0.0;
+			double norm2 = 0.0;
+			for (int i = 0; i < vector1.length; i++) {
+				dotProduct += vector1[i] * vector2[i];
+				norm1 += Math.pow(vector1[i], 2);
+				norm2 += Math.pow(vector2[i], 2);
+			}
+			return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
 		});
-		
+
 		cf.printRecommendation();
 
 	}
