3b1d063b61683ec12bdac3916ab7e2da58714393
subui <subui.bk@gmail.com>
2018-01-04 13:16:54 +0700

    src\Main.java

--- src/Main.java
+++ src/Main.java
@@ -30,14 +30,14 @@ public class Main {
 			@Override
 			public double calculate(double[] vector1, double[] vector2) {
 				double dotProduct = 0.0;
-			    double normA = 0.0;
-			    double normB = 0.0;
+			    double norm1 = 0.0;
+			    double norm2 = 0.0;
 			    for (int i = 0; i < vector1.length; i++) {
 			        dotProduct += vector1[i] * vector2[i];
-			        normA += Math.pow(vector1[i], 2);
-			        normB += Math.pow(vector2[i], 2);
+			        norm1 += Math.pow(vector1[i], 2);
+			        norm2 += Math.pow(vector2[i], 2);
 			    }   
-			    return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+			    return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
 			}
 		});
 		
